15a2a42fa1742ff671c80ff27650f58c024d8161
Blue Demon9 <94703532+Blue-demon9@users.noreply.github.com>
2022-01-12 03:40:58 -0500

    Add files via upload

---
+++ Flash.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <unistd.h>  // for sleep function
+using namespace std;
+
+void change();
+
+int main(){
+    int sec,t;
+    system("clear");
+    system("echo  \" \033[01m \033[34m \" ");
+    system("figlet   \"Flash Ip Changer\" ");
+
+    cout<<"\033[01m\033[34m\n[*] Current ip address:"<<endl;
+    system("curl myexternalip.com/raw");
+    cout<<"\033[34m\n\nIn order to use this tool change your  SOCKES to \033[33m127.0.0.1:9050 "<<endl;
+    system("service tor start");
+    cout<<"\033[34m\n\a[*] Enter the time to change Ip in Sec\n\033[94m  >> ";
+    cin>>sec;
+    cout<<"\033[34m\a[*] Enter how many times you want the task to repeat \033[33m(type 0 for infinite)\n\033[94m  >>";
+    cin>>t;
+    cout<<"\n\n";
+
+    if(t ==0){
+        for(; ;){
+            try{
+                sleep(sec);
+                change();
+            }
+            catch(...){
+                cout<<"\nAuto tor is closing ....."<<endl;
+                exit(1);
+            }
+        }
+    }
+    else{
+        for(int i=0;i<t;i++){
+            sleep(sec);
+            change();
+	    cout<<endl;
+        }
+    }
+
+
+}
+
+void change(){
+    system("service tor reload");
+    system("curl --preproxy socks5://127.0.0.1:9050  \"https://www.myexternalip.com/raw\"");
+
+}
---
+++ install.sh
@@ -0,0 +1,7 @@
+clear
+chmod +x Flash
+cp Flash /usr/bin
+echo -e "\033[94m\033[01m[+]Installing Flash Ip Changer...\n\n\033[32m"
+apt-get install figlet tor curl
+echo -e "\n\n\a\\033[94m[*] Flash Ip Changer has been installed successfully."
+echo -e "\033[94m[*] You can run it by typing \033[32m'Flash'.\n"
